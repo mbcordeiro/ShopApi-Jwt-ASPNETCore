18a32e468128f8abab1ce3379b5d958a2805bb0b
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-27 01:06:35 -0300

    Controller

---
+++ ShopApi/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShopApi.Models;
+using ShopApi.Repositories;
+using ShopApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApi.Controllers
+{
+    [Route("v1/account")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.Username, model.Password);
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+            var token = TokenService.GenerateToken(user);
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
