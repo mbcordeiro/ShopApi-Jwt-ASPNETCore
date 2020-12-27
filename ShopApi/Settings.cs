aa1ac5b51b5d58768062f1ea6fadb748688383e0
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-26 23:01:07 -0300

    First commit

---
+++ ShopApi/Settings.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApi
+{
+    public class Settings
+    {
+        public static string Secret = "d1e8a70b5ccab1dc2f56bbf7e99f064a660c08e361a35751b9c483c88943d082";
+    }
+}
